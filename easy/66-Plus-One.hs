50a14dc4b4e2b5e51ac1662b07fb6c85b883867a
Dom Binks <dominic.binks@gmail.com>
2022-09-05 00:29:27 +0100

    Fix comment in 66-Plus-One.hs

--- easy/66-Plus-One.hs
+++ easy/66-Plus-One.hs
@@ -3,10 +3,10 @@ recur :: [Int] -> [Int] -> [Int]
 -- Singleton list case
 recur (x:[]) [] = if x > 9 then [1,x-10] -- If digit is above 9
                   else [x] -- If digit is below 10
--- Base case of non-singleton lists
+-- Base case for non-singleton lists
 recur (x:[]) ys = if x > 9 then [1,x-10] ++ ys -- If digit is above 9
                   else x:ys -- If digit is below 10
--- Recursive case of non-singleton lists
+-- Recursive case for non-singleton lists
 recur xs ys = if (head (reverse xs)) > 9 then recur ((init (init xs)) ++ [((reverse xs) !! 1) + 1]) ([0] ++ ys) -- If digit is above 9
               else recur (init xs) ([(head (reverse xs))] ++ ys) -- If digit is below 10
 
