4cce5435d25d4df734b088558e43bfe082da2d1f
Dom Binks <dominic.binks@gmail.com>
2022-10-01 17:11:05 +0100

    Add comments to 1512

--- easy/1512-Number-of-Good-Pairs.cpp
+++ easy/1512-Number-of-Good-Pairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int out = 0;
-        for(int i = 0; i < nums.size(); i++)   
+        int out = 0; // Number of good pairs
+
+        // For each pair where i < j
+        for(int i = 0; i < nums.size(); i++)
         {
             for(int j = i+1; j < nums.size(); j++)
             {
                 if(nums[i] == nums[j] && i < j) 
                 {
-                    out++;
+                    out++; // Increment out if it's a good pair
                 }
             }
         }
