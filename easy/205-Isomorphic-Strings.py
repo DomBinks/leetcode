5e6463b7834c381d2e9a34d751ad004461780ed8
Dom Binks <dominic.binks@gmail.com>
2023-01-06 00:22:28 +0000

    Add comments to 205

--- easy/205-Isomorphic-Strings.py
+++ easy/205-Isomorphic-Strings.py
@@ -1,16 +1,22 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # Map of characters in string s to characters in string t
         map = {}
         
+        # For all characters
         for i in range(len(s)):
+            # If the character in s already has a mapping
             if s[i] in map:
+                # If the mapping doesn't map to the value in t
                 if map[s[i]] != t[i]:
                     return False
             
+            # If the character in t already has a mapping to it
             elif t[i] in map.values():
                 return False
 
             else:
+                # Add the mapping from the character in s to the character in t
                 map[s[i]] = t[i]
 
         return True
